e314d96c6e25d8e595a316dc0f34881d95ecf94f
andreyanez <andre.yanez22@gmail.com>
2023-01-05 15:36:17 -0500

    Made the toggles more...fun

--- project-checkboxes/script.js
+++ project-checkboxes/script.js
@@ -2,12 +2,19 @@ const toggles = document.querySelectorAll('.toggle');
 const good = document.querySelector('#good');
 const cheap = document.querySelector('#cheap');
 const fast = document.querySelector('#fast');
+const container = document.querySelector('#toggleContainers');
 
 toggles.forEach(toggle => toggle.addEventListener('change', e => doTheTrick(e.target)));
 
 function doTheTrick(theClickedOne) {
 	if (good.checked && cheap.checked && fast.checked) {
-		alert('Yeah right');
+		const msg = document.createElement('p');
+		msg.style.textAlign = 'center';
+		msg.innerText = 'yeah right.';
+		container.appendChild(msg);
+		setTimeout(() => {
+			msg.remove();
+		}, 500);
 		if (good === theClickedOne) {
 			fast.checked = false;
 		}
